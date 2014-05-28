4bd6201f1afe52abd5c64aaa92ce0a4f578deef5
Alf Eaton <alf@hubmed.org>
2014-05-28 21:36:55 +0100

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -7,10 +7,3 @@ $config = json_decode(file_get_contents('config.json'), true);
 $client = new FiguresHandler($config, __DIR__ . '/data/');
 
 $client->fetch_feed('https://peerj.com/articles/index.json');
-
-/*
-$client = new FlickrClient($config);
-$status = $client->check_status();
-print_r($status);
-exit();
-*/
